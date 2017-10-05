556aee148d7d09001b75519b3b315caad8fd5795
Shekhar Singh <shekhar.singh087@gmail.com>
2017-10-05 15:08:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shekh/fragmments/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.shekh.fragmments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    LinearLayout linearLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
